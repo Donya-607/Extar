b0864a0803184e3759df8a24271c70e9a58a9375
Donya <alias>
2019-02-21 11:25:44 +0900

    Starファイルを追加

---
+++ Star.cpp
@@ -0,0 +1,30 @@
+#include "DxLib.h"
+#include "Common.h"
+#include "Music.h"
+
+#include "Star.h"
+
+void Star::Init( int Row, int Column, int Width, int Height, int Level )
+{
+	row		= Row;
+	column	= Column;
+
+	width	= Width;
+	height	= Height;
+
+	level	= Level;
+}
+void Star::Uninit()
+{
+
+}
+
+void Star::Update()
+{
+
+}
+
+void Star::Draw( Vector2 shake )
+{
+
+}
---
+++ Star.h
@@ -0,0 +1,30 @@
+#ifndef INCLUDED_STAR_H_
+#define INCLUDED_STAR_H_
+
+#include "Vector2.h"
+
+class Star
+{
+private:
+	int row;	// 行, 0始まり
+	int column;	// 列, 0始まり
+
+	int width;	// マス目式の大きさ, 1始まり
+	int height;	// マス目式の大きさ, 1始まり
+
+	int level;	// 1 ~ 6
+public:
+	Star() : row( 0 ), column( 0 ), width( 1 ), height( 1 ),
+		level( 6 )
+	{}
+	~Star() {}
+
+	void Init( int row, int column, int width, int height, int level );
+	void Uninit();
+
+	void Update();
+
+	void Draw( Vector2 shake );
+};
+
+#endif //INCLUDED_STAR_H_
