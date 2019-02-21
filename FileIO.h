a5f023cce9b5b60b61e111580c3741c3760e2a87
Donya <alias>
2019-02-21 15:57:48 +0900

    ファイル関係用のファイルを追加

---
+++ FileIO.h
@@ -0,0 +1,35 @@
+#ifndef INCLUDED_LOADFILE_H_
+#define INCLUDED_LOADFILE_H_
+
+#include <vector>
+
+#include "Common.h"
+
+#if USE_IMGUI
+
+#include <imgui.h>
+#include <imgui_impl_dxlib.h>
+
+#endif // USE_IMGUI
+
+class Camera;
+class Star;
+
+namespace FileIO
+{
+	void ReadAllCamera();
+	void ReadAllStars();
+
+	void WriteCamera( int stageNumber, const Camera *data );
+	int  GetCameraArraySize();
+	Camera FetchCameraInfo( int stageNumber );
+
+	void WriteStars( int stageNumber, const std::vector<Star> *data );
+	int  GetStarsArraySize( int stageNumber );
+	Star FetchStarsInfo( int stageNumber, int index );
+
+	void ReleaseCameraData();
+	void ReleaseStarsData();
+}
+
+#endif //INCLUDED_LOADFILE_H_
