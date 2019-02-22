36c7f9c83fd4d15d6dc3b2f62adb61f2958f01bd
Donya <alias>
2019-02-22 12:34:18 +0900

    Keyboard.hにShiftとCtrlのマクロを作成

--- Keyboard.h
+++ Keyboard.h
@@ -11,11 +11,10 @@
 #define TRG( x )		( 1 == GetKeyboard( x ) )
 
 // キーを押しているかどうか
-#define PRESS( x )	( GetKeyboard( x ) )
+#define PRESS( x )		( GetKeyboard( x ) )
 
-// 使用するボタンのマクロ
-#define BUTTON_ACCEL_K		( KEY_INPUT_Z )
-#define BUTTON_ATTACK_K		( KEY_INPUT_X )
+#define PRESS_CTRL		( PRESS( KEY_INPUT_LCONTROL ) || PRESS( KEY_INPUT_RCONTROL ) )
+#define PRESS_SHIFT		( PRESS( KEY_INPUT_LSHIFT )   || PRESS( KEY_INPUT_RSHIFT ) )
 
 int  GetKeyboard( const int KeyCode );
 void KeyboardUpdate();
