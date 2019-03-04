fcb53177205c81b4ad77231f7b4f9ef50b4db530
Donya <alias>
2019-03-04 14:47:35 +0900

    これから，残像を出すほうの流れ星を作る

---
+++ ShootingStar.cpp
@@ -0,0 +1,199 @@
+#include "DxLib.h"
+
+#include "ShootingStar.h"
+
+namespace ShootingStarImage
+{
+	static int hStar;
+
+	void Load()
+	{
+		// Ç∑Ç≈Ç…ílÇ™ì¸Ç¡ÇƒÇ¢ÇΩÇÁÅCì«Ç›çûÇÒÇæÇ‡ÇÃÇ∆Ç›Ç»ÇµÇƒîÚÇŒÇ∑
+		if ( 0 != hStar )
+		{
+			return;
+		}
+		// else
+
+		hStar = LoadGraph( "./Data/Images/Star/ShootingStar.png" );
+	}
+
+	void Release()
+	{
+		DeleteGraph( hStar );
+		hStar = 0;
+	}
+
+	int  GetHandle()
+	{
+		return hStar;
+	}
+}
+
+void VisionStar::Init( int imageHandle, Vector2 centerPos )
+{
+	handle	= imageHandle;
+	pos		= centerPos;
+}
+
+void VisionStar::Uninit()
+{
+
+}
+
+void VisionStar::Update()
+{
+	timer++;
+}
+
+void VisionStar::Draw( Vector2 shake ) const
+{
+	DrawRotaGraph
+	(
+		scast<int>( pos.x ),
+		scast<int>( pos.y ),
+		1.0,
+		ToRadian( angle ),
+		handle,
+		TRUE
+	);
+}
+
+bool VisionStar::IsDisappear() const
+{
+	constexpr int EXIST_TIME = 30;
+
+	return ( EXIST_TIME <= timer ) ? true : false;
+}
+
+
+
+void ShootingStar::Init( Vector2 centerPos )
+{
+	pos = centerPos;
+
+	constexpr float SPD = 10.0f;
+	velo.x = -SPD;
+	velo.y = SPD;
+}
+
+void ShootingStar::Uninit()
+{
+
+}
+
+void ShootingStar::Update()
+{
+	timer++;
+
+	pos += velo;
+}
+
+void ShootingStar::Draw( Vector2 shake ) const
+{
+	DrawRotaGraph
+	(
+		scast<int>( pos.x ),
+		scast<int>( pos.y ),
+		1.0,
+		ToRadian( angle ),
+		ShootingStarImage::GetHandle(),
+		TRUE
+	);
+}
+
+bool ShootingStar::IsRequestingGenerateVision() const
+{
+	constexpr int GENERATE_INTERVAL = 2;
+	return ( ( timer % GENERATE_INTERVAL ) == 1 ) ? true : false;
+}
+
+bool ShootingStar::IsDisappear() const
+{
+	constexpr int EXIST_TIME = 30;
+
+	return ( EXIST_TIME <= timer ) ? true : false;
+}
+
+int ShootingStar::GetHandle() const
+{
+	return ShootingStarImage::GetHandle();
+}
+
+
+
+void ShootingStarMng::Init()
+{
+	stars.clear();
+}
+
+void ShootingStarMng::Uninit()
+{
+	stars.clear();
+}
+
+void ShootingStarMng::Update()
+{
+	for (
+			std::vector<ShootingStar>::iterator &it = stars.begin();
+			it != stars.end();
+		)
+	{
+		it->Update();
+
+		if ( it->IsRequestingGenerateVision() )
+		{
+			GenerateVision( it->GetHandle(), it->GetPos() );
+		}
+
+		if ( it->IsDisappear() )
+		{
+			it = stars.erase( it );
+			continue;
+		}
+		// else
+
+		it++;
+	}
+
+	for (
+			std::vector<VisionStar>::iterator &it = visions.begin();
+			it != visions.end();
+		)
+	{
+		it->Update();
+
+		if ( it->IsDisappear() )
+		{
+			it = visions.erase( it );
+			continue;
+		}
+		// else
+
+		it++;
+	}
+}
+
+void ShootingStarMng::Draw( Vector2 shake ) const
+{
+	for ( const VisionStar &it : visions )
+	{
+		it.Draw( shake );
+	}
+	for ( const ShootingStar &it : stars )
+	{
+		it.Draw( shake );
+	}
+}
+
+void ShootingStarMng::GenerateStar( Vector2 centerPos )
+{
+	stars.push_back( ShootingStar() );
+	stars.back().Init( centerPos );
+}
+
+void ShootingStarMng::GenerateVision( int imageHandle, Vector2 centerPos )
+{
+	visions.push_back( VisionStar() );
+	visions.back().Init( imageHandle, centerPos );
+}
---
+++ ShootingStar.h
@@ -0,0 +1,116 @@
+#ifndef INCLUDED_STAR_H_
+#define INCLUDED_STAR_H_
+
+#include <vector>
+
+#include "Common.h"
+
+#if USE_IMGUI
+
+#include <imgui.h>
+#include <imgui_impl_dxlib.h>
+
+#endif // USE_IMGUI
+
+#include "Vector2.h"
+
+namespace ShootingStarImage
+{
+	void Load();
+	void Release();
+
+	int  GetHandle();
+}
+
+class VisionStar
+{
+private:
+	int		timer;
+	int		handle;
+	float	angle;	// Degree, ClockWise
+
+	Vector2	pos;	// Center
+public:
+	VisionStar() : timer( 0 ), handle( 0 ), angle( 0 ), pos() {}
+	~VisionStar() {}
+
+	void Init( int imageHandle, Vector2 pos );
+	void Uninit();
+
+	void Update();
+
+	void Draw( Vector2 shake ) const;
+public:
+	bool IsDisappear() const;
+};
+
+class ShootingStar
+{
+private:
+	int		timer;
+	float	angle;	// Degree, ClockWise
+
+	Vector2	pos;	// Center
+	Vector2 velo;	// Velocity
+public:
+	ShootingStar() :
+		timer( 0 ),
+		angle( 0 ),
+		pos(), velo()
+	{}
+	~ShootingStar() {}
+
+	void Init( Vector2 centerPos );
+	void Uninit();
+
+	void Update();
+	void Draw( Vector2 shake ) const;
+public:
+	bool IsRequestingGenerateVision() const;
+	bool IsDisappear() const;
+public:
+	int GetHandle() const;
+	Vector2 GetPos() const
+	{
+		return pos;
+	}
+};
+
+class ShootingStarMng
+{
+private:
+	std::vector<ShootingStar> stars;
+	std::vector<VisionStar> visions;
+
+#if USE_IMGUI
+
+#endif // USE_IMGUI
+
+public:
+	ShootingStarMng() : stars(), visions() {}
+	~ShootingStarMng()
+	{
+		std::vector<ShootingStar>().swap( stars );
+	}
+
+	void Init();
+	void Uninit();
+
+	void Update();
+
+	void Draw( Vector2 shake ) const;
+public:
+	void GenerateStar( Vector2 centerPos );
+private:
+	void GenerateVision( int imageHandle, Vector2 centerPos );
+public:
+	int  GetArraySize() const
+	{
+		return scast<int>( stars.size() );
+	}
+#if USE_IMGUI
+
+#endif // USE_IMGUI
+};
+
+#endif //INCLUDED_STAR_H_
